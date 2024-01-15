b7b318b459a2d0d98dc6f31f143907fbfee04853
Ihor Shevchyk <shevchyk.ihor@gmail.com>
2024-01-15 19:42:35 +0200

    first commit

---
+++ gulp/tasks/java-script.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import webpack from 'webpack-stream';
+import { webpackConfig } from '../../webpack.config.js';
+import { plugins } from '../config/plugins.js';
+import { filePaths } from '../config/paths.js';
+import { logger } from "../config/logger.js";
+import $ from 'jquery';
+
+const javaScript = async (isDev) => {
+  return gulp.src(filePaths.src.js)
+    .pipe(logger.handleError('JS'))
+    .pipe(webpack({
+      config: await webpackConfig(isDev),
+      resolve: {
+        alias: {
+          'jquery': 'jquery',
+        },
+      },
+    }))
+    .pipe(gulp.dest(filePaths.build.js))
+    .pipe(plugins.browserSync.stream());
+};
+
+export { javaScript };
