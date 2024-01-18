14f19ec54b40bddf4cca4f5881f3cca5caa9aed1
Ihor Shevchyk <shevchyk.ihor@gmail.com>
2024-01-19 01:34:38 +0200

    dev-ihor

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import Swiper from 'swiper';
 
 // $(function () {
 //     $.ajax({
@@ -6,11 +7,11 @@ import $ from 'jquery';
 //         method: 'GET',
 //         dataType: 'json',
 //         success: function(data) {
-//             $.each(data.data, function(index, obj) {
-//                 let imgURL = obj.attributes.img.data.attributes.url;
-//                 $('<img>').attr('src', imgURL).appendTo('.img-con');
-//             });     
-            
+//             // $.each(data.data, function(index, obj) {
+//             //     let imgURL = obj.attributes.img.data.attributes.url;
+//             //     $('<img>').attr('src', imgURL).appendTo('.img-con');
+//             // });     
+
 //             // При необхідності вивести на сторінку
 //             // response.forEach(function(card) {
 //             //     $('body').append('<p>' + JSON.stringify(card) + '</p>');
@@ -21,3 +22,27 @@ import $ from 'jquery';
 //         }
 //     });
 // })
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  initialSlide: 3,
+  // spaceBetween: 10,
+  // loop: true,
+});
+
+// cards.forEach(function (card, index) {
+//   card.addEventListener('click', function () {
+//     swiper.slideTo(index); // Переміщаємо слайдер на вказаний індекс
+//   });
+// });
+
+var slides = document.querySelectorAll('.swiper-slide');
+
+// Додавання обробників подій на кожен слайд
+slides.forEach(function (slide, index) {
+  slide.addEventListener('click', function () {
+    // Зміна активного слайда при натисканні
+    swiper.slideTo(index);
+  });
+});
