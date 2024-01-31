575a9cea56d92d77aaeb8ebefa5adf662bf4c5b4
Maksym Sharavin <maksimsharavin13@gmail.com>
2024-01-31 22:36:41 +0100

    Artist

---
+++ src/js/Premium.js
@@ -0,0 +1,46 @@
+import $ from 'jquery';
+$('.premium__card ').removeClass('.active');
+$('.premium__card ').hover(
+  function () {
+    $(this).addClass('active');
+  },
+  function () {
+    $(this).removeClass('active');
+  }
+);
+window.changeCard = function (premiumType) {
+  $('.premium__card').hide();
+  $('.premium__buttons--btn').removeClass('active');
+  $('#' + premiumType).show();
+  $('.premium__buttons--btn[data-type="' + premiumType + '"]').addClass(
+    'active'
+  );
+};
+
+$(document).ready(function () {
+  function toggleCards() {
+    let width = $(window).width();
+    if (width <= 650) {
+      $('.premium__card').hide();
+      $('.premium__buttons--btn').removeClass('active');
+      $('#premiumWeek').show();
+      $('.premium__buttons--btn[data-type="premiumWeek"]').addClass('active');
+    } else {
+      $('.premium__card').show();
+    }
+  }
+
+  toggleCards();
+
+  $(window).resize(function () {
+    toggleCards();
+  });
+
+  $('.premium__buttons--btn').click(function () {
+    let type = $(this).data('type');
+    $('.premium__buttons--btn').removeClass('active');
+    $(this).addClass('active');
+    $('.premium__card').hide();
+    $('#' + type).show();
+  });
+});
